1344811cd6c972f65d33f3b3d53cf1be899b971a
aman <aman@onjoyride.com>
2024-12-30 14:19:09 +0530

    removed unwanted code

--- scripts/V1/addToken.js
+++ scripts/V1/addToken.js
@@ -1,11 +1,6 @@
 const { ethers  } = require('hardhat');
 
 async function main(){
-    accounts = await ethers.getSigners();
-    owner = accounts[0];
-    console.log("selfAddress : ", owner.address);
-    const selfAddress = owner.address;
-
     const TokenFactory = await ethers.getContractFactory("CustomToken");
     const initialSupply = ethers.utils.parseUnits("1000000", 18); // 1000000 * 10 ** 18 initial supply
     const token = await TokenFactory.deploy("MyToken2", "MTK2", initialSupply); 
