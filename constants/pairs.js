0ea9eadf68076f106bee9259ba60011587f261fe
aman <aman@onjoyride.com>
2024-12-30 16:41:55 +0530

    added token pais

--- constants/pairs.js
+++ constants/pairs.js
@@ -1,4 +1,4 @@
-import { tokenOne, tokenTwo } from "../scripts/V1/dexAbi";
+import { tokenEight, tokenNine, tokenOne, tokenTen, tokenThree, tokenTwo } from "../scripts/V1/dexAbi";
 
 export const deployedPairs = [
     {
@@ -6,5 +6,29 @@ export const deployedPairs = [
         token1: tokenTwo,
         stable: false,
         pairAddress: "0x1c2b9eb0a6c13e7d21f9915bea738e4d7a24c358"
+    },
+    {
+        token0: tokenThree,
+        token1: tokenTwo,
+        stable: false,
+        pairAddress: "0xc6e39a293117881cbf6156d1616bb362121bbb0a"
+    },
+    {
+        token0: tokenOne,
+        token1: tokenEight,
+        stable: false,
+        pairAddress: "0xc6e39a293117881cbf6156d1616bb362121bbb0a"
+    },
+    {
+        token0: tokenOne,
+        token1: tokenTen,
+        stable: false,
+        pairAddress: "0x285e4B9137c2e0D482A044Db0B1c1E8616fa8c3e"
+    },
+    {
+        token0: tokenTwo,
+        token1: tokenNine,
+        stable: true,
+        pairAddress: "0x91De570eCb5266aDf0767ce7c0488d43D3f46E97"
     },
 ]
