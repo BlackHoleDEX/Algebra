7e69ced713ab6f821e609f1791d5fe82361f8cfb
aman <aman@onjoyride.com>
2025-01-16 13:00:07 +0530

    auto writing in json file logic

--- scripts/blackhole-scripts/getAllPairApi.js
+++ scripts/blackhole-scripts/getAllPairApi.js
@@ -4,32 +4,49 @@ const { ZERO_ADDRESS } = require("@openzeppelin/test-helpers/src/constants.js");
 const { blackHolePairApiV2Abi, blackHolePairApiV2ProxyAddress } = require('./pairApiConstants');
 const { pairFactoryAbi, pairFactoryAddress } = require('../V1/dexAbi');
 const { voterV3Abi, voterV3Address } = require('./gaugeConstants/voter-v3');
-
-
+const fs = require("fs");
 
 async function main () {
     accounts = await ethers.getSigners();
     owner = accounts[0]
 
     const voterV3Contract = await ethers.getContractAt(voterV3Abi, voterV3Address);
-    console.log("voterV3Contract ", voterV3Address, voterV3Contract.address);
-    console.log("voterV3: ", await voterV3Contract._epochTimestamp());
 
     const pairFactoryContract = await ethers.getContractAt(pairFactoryAbi, pairFactoryAddress);
     const pairsLength = await pairFactoryContract.allPairsLength();
 
-    console.log("pairsLength : ", pairsLength);
+    // console.log("pairsLength : ", pairsLength);
 
     const blackHoleAllPairContract = await ethers.getContractAt(blackHolePairApiV2Abi, blackHolePairApiV2ProxyAddress);
     const blackHoleAllPairContractOwner = await blackHoleAllPairContract.owner();
-    console.log("blackHoleAllPairContract owner : ", blackHoleAllPairContractOwner);
-    console.log('get all pair inputs', owner.address, BigInt(pairsLength), BigInt(0))
+    // console.log("blackHoleAllPairContract owner : ", blackHoleAllPairContractOwner);
+    // console.log('get all pair inputs', owner.address, BigInt(pairsLength), BigInt(0))
     const  blackHoleAllPairContractPairsData = await blackHoleAllPairContract.getAllPair(owner.address, BigInt(pairsLength), BigInt(0));
-    // const totalPairs = blackHoleAllPairContractPairsData[0];
-    // const pairs = blackHoleAllPairContractPairsData[1];
-    // console.log("Total pairs : ", totalPairs);
-    console.log("All pairs : ", blackHoleAllPairContractPairsData);
 
+    // console.log("All pairs : ", blackHoleAllPairContractPairsData);
+
+
+    let list = []
+    try {
+        blackHoleAllPairContractPairsData.forEach(elm => {
+            let i = 1;
+            const keys = Object.keys(elm);
+            let mp = {};
+            keys.forEach(key => {
+                if (isNaN(parseInt(key))){
+                    mp[key] = elm[key];
+                }
+            })
+            list.push(mp);
+        })
+
+        const outputPath = './blackHolePairsData.json';
+        fs.writeFileSync(outputPath, JSON.stringify(list, null, 2));
+        console.log(`Data written to ${outputPath}`);
+
+    } catch (error) {
+        console.error("Error fetching pairs data: ", error);
+    }
 }
 
 main()
