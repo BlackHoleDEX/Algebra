d684efd6d4c6f489446e7768dd4f58cc1621cadf
Mitesh <mgupta@kiwiup.com>
2025-02-10 12:49:01 +0530

    locking give 10% more voting power.

--- contracts/VotingEscrow.sol
+++ contracts/VotingEscrow.sol
@@ -733,7 +733,11 @@ contract VotingEscrow is IERC721, IERC721Metadata, IVotes {
         LockedBalance memory old_locked;
         (old_locked.amount, old_locked.end, old_locked.isPermanent, old_locked.isSMNFT) = (_locked.amount, _locked.end, _locked.isPermanent, _locked.isSMNFT);
         // Adding to existing lock, or if a lock is expired - creating a new one
-        _locked.amount += int128(int256(_value));
+        if(old_locked.isSMNFT) {
+            _locked.amount += int128(int256(((110*_value)/100)));
+        } else {
+            _locked.amount += int128(int256(_value));
+        }
            
         if (unlock_time != 0) {
             _locked.end = unlock_time;
