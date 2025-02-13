cdbff72693fc2b46267446c55af8f843605deb54
IliaAzhel <aje-ilia2013@yandex.by>
2025-02-13 20:12:43 +0300

    add camelot plugin deployment script

---
+++ src/plugin/scripts/deployCamelotPluginFactory.js
@@ -0,0 +1,36 @@
+const hre = require("hardhat");
+const fs = require('fs');
+const path = require('path');
+
+async function main() {
+
+    const deployDataPath = path.resolve(__dirname, '../../../deploys.json')
+    const deploysData = JSON.parse(fs.readFileSync(deployDataPath, 'utf8'))
+
+    const CamelotBasePluginFactory = await hre.ethers.getContractFactory("CamelotBasePluginFactory");
+    const dsFactory = await CamelotBasePluginFactory.deploy(deploysData.factory);
+
+    await dsFactory.waitForDeployment()
+
+    console.log("PluginFactory to:", dsFactory.target);
+
+    await dsFactory.changeDynamicFeeStatus(true);
+    
+    const factory = await hre.ethers.getContractAt('IAlgebraFactory', deploysData.factory)
+
+    await factory.setDefaultPluginFactory(dsFactory.target)
+    console.log('Updated plugin factory address in factory')
+
+    deploysData.CamelotBasePluginFactory = dsFactory.target;
+    fs.writeFileSync(deployDataPath, JSON.stringify(deploysData), 'utf-8');
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
