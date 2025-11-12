d34a0b4737291c49f1a7fd655ad5d01371f72424
Sandeep <sandeep@onjoyride.com>
2025-11-12 20:40:55 +0530

    Refactor BasePluginV3Factory by removing createPluginForExistingPool function and updating interface to reflect the new createPluginForExistingCustomPool function. This change enhances clarity and enforces access control for custom pool deployments.

--- src/plugin/contracts/BasePluginV3Factory.sol
+++ src/plugin/contracts/BasePluginV3Factory.sol
@@ -49,17 +49,6 @@ contract BasePluginV3Factory is IBasePluginV3Factory {
     require(msg.sender == algebraFactory);
   }
 
-  /// @inheritdoc IBasePluginV3Factory
-  function createPluginForExistingPool(address token0, address token1) external override returns (address) {
-    IAlgebraFactory factory = IAlgebraFactory(algebraFactory);
-    require(factory.hasRoleOrOwner(factory.POOLS_ADMINISTRATOR_ROLE(), msg.sender));
-
-    address pool = factory.poolByPair(token0, token1);
-    require(pool != address(0), 'Pool not exist');
-
-    return _createPlugin(pool);
-  }
-
   function createPluginForExistingCustomPool(address token0, address token1, address customPoolDeployer) external returns (address) {
     IAlgebraFactory factory = IAlgebraFactory(algebraFactory);
     require(msg.sender == customPoolDeployer || factory.hasRoleOrOwner(factory.POOLS_ADMINISTRATOR_ROLE(), msg.sender), 'Only deployer or admin');
