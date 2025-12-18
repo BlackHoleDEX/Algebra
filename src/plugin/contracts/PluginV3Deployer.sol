2af42fe11320c1b725603342dc9bef49d6914327
Sandeep <sandeep@onjoyride.com>
2025-12-18 18:14:58 +0530

    Implement IPluginV3Deployer interface in PluginV3Deployer contract

--- src/plugin/contracts/PluginV3Deployer.sol
+++ src/plugin/contracts/PluginV3Deployer.sol
@@ -2,11 +2,12 @@
 pragma solidity =0.8.20;
 
 import './AlgebraBasePluginV3.sol';
+import './interfaces/IPluginV3Deployer.sol';
 
 /// @title PluginV3Deployer
 /// @notice Helper contract to deploy `AlgebraBasePluginV3` instances
 /// @dev Kept minimal so that the main factory (`BasePluginV3Factory`) can stay below bytecode size limits
-contract PluginV3Deployer {
+contract PluginV3Deployer is IPluginV3Deployer {
   function deployPlugin(
     address pool,
     address algebraFactory,
