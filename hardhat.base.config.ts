2a0f393f7821c7e8774a5b2271825f7f0f22a220
Lokendra Surya <lokendra@onjoyride.com>
2026-01-08 14:18:46 +0530

    Added devent deploy json and updated npm packages

--- hardhat.base.config.ts
+++ hardhat.base.config.ts
@@ -91,6 +91,11 @@ export default {
       chainId: 11155111,
       accounts: [`0x${MNEMONIC}`],
     },
+    sepoliaDevnet: {
+      url: `https://bitter-solitary-frost.ethereum-sepolia.quiknode.pro/ca090f980f3013fc7d11ecfb8812b0a99894f408/`,
+      chainId: 11155111,
+      accounts: [`0x${MNEMONIC}`],
+    },
     blastTestnet: {
       url: `https://blast-sepolia.blockpi.network/v1/rpc/public`,
       chainId: 168587773,
@@ -161,6 +166,14 @@ export default {
       },
       {
         network: 'sepoliaTestnet',
+        chainId: 11155111,
+        urls: {
+          apiURL: 'https://api.etherscan.io/v2/api?chainid=11155111',
+          browserURL: 'https://sepolia.etherscan.io',
+        },
+      },
+      {
+        network: 'sepoliaDevnet',
         chainId: 11155111,
         urls: {
           apiURL: 'https://api.etherscan.io/v2/api?chainid=11155111',
