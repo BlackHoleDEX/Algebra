eae1552b276d851411f1d983c4bc4101b01d85df
Sandeep <sandeep@onjoyride.com>
2026-04-27 23:06:29 +0530

    Update NFT naming in NonfungiblePositionManager and NFTDescriptor contracts to version 2

--- src/periphery/contracts/NonfungiblePositionManager.sol
+++ src/periphery/contracts/NonfungiblePositionManager.sol
@@ -103,7 +103,7 @@ contract NonfungiblePositionManager is
         address _tokenDescriptor_,
         address _poolDeployer
     )
-        ERC721Permit('Blackhole Positions NFT-V1', 'BH-POS', '1')
+        ERC721Permit('Blackhole Positions NFT-V2', 'BH-POS', '2')
         PeripheryImmutableState(_factory, _WNativeToken, _poolDeployer)
     {
         _tokenDescriptor = _tokenDescriptor_;
